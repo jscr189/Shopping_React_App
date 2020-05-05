7be80ad65f3a38a753addaa04b3faf098c8bae07
jscr189 <jscr189@gmail.com>
2020-05-05 21:01:17 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import HomePage from './HomePage/homepage.component';
+
+class App extends React.Component {
+  render(){
+    return <HomePage/>
+  }
+}
+
+export default App;
