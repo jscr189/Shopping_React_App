fbba8f6bf5aef86b40640fcc2a3243f936048758
jscr189 <jscr189@gmail.com>
2020-05-06 23:58:21 +0530

    update menu items with Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route,Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
-class App extends React.Component {
-  render(){
-    return <HomePage/>
-  }
+const HatsPage= () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
+function App () {
+      return(<div>
+        <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route path='/shop/hats' component={HatsPage}/>
+        </Switch>
+      </div>);
 }
 
 export default App;
