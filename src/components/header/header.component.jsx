628802d32ba69a822551d44bf0ecdd58f79e87df
jscr189 <jscr189@gmail.com>
2020-06-25 00:18:45 +0530

    using reselect memorization, added checkout-item component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import { selectCartHidden } from './../../redux/cart/cart.selectors';
+import { selectCurrentUser } from './../../redux/user/user.selectors';
+
+import { createStructuredSelector } from 'reselect';
+
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
@@ -27,9 +32,9 @@ const Header = ({ currentUser, hidden })=>(
     </div>
 )
 
-const mapStateToProps = ({ user :{ currentUser }, cart : { hidden } }) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser : selectCurrentUser,
+    hidden : selectCartHidden
 })
 
 export default connect(mapStateToProps)(Header);
